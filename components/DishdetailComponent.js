3af8870c5e098e3be14cb66681aa8bf66acc4909
hsportilla <hsportilla@gmail.com>
2020-12-08 23:39:33 -0500

    react native final

--- components/DishdetailComponent.js
+++ components/DishdetailComponent.js
@@ -1,23 +1,108 @@
 import React, {Component} from 'react';
-import {View,Text} from 'react-native';
-import {Card} from 'react-native-elements';
-import {DISHES} from '../shared/dishes';
+import {View,Text,ScrollView,FlatList,Modal,StyleSheet,Button,PanResponder,Alert, Share} from 'react-native';
+import {Card, Icon, Rating, Input} from 'react-native-elements';
+import { connect } from 'react-redux';
+import { baseUrl } from '../shared/baseUrl';
+import { postFavorite } from '../redux/ActionCreators';
+import { postComment } from '../redux/ActionCreators';
+import * as Animatable from 'react-native-animatable';
 
- function RenderDish (props) {
+const mapStateToProps = state => {
+    return {
+      dishes: state.dishes,
+      comments: state.comments,
+      favorites: state.favorites
+    }
+}
+
+const mapDispatchToProps = dispatch => ({
+    postFavorite: (dishId) => dispatch(postFavorite(dishId)),
+    postComment: (comment) => dispatch(postComment(comment))
+})
+
+function RenderDish (props) {
     const dish = props.dish;
+
+    handleViewRef = ref => this.view = ref
+
+    const recognizeDrag = ({ moveX, moveY, dx, dy }) => {
+        if ( dx < -200 )
+            return true;
+        else
+            return false;
+    }
+
+    const recognizeComment = ({ moveX, moveY, dx, dy }) => {
+        return (dx > 200);
+    };
+
+    const shareDish = (title, message, url) => {
+        Share.share({
+            title: title,
+            message: title + ': ' + message + ' ' + url,
+            url: url
+        },{
+            dialogTitle: 'Share ' + title
+        })
+    }
+
+    const panResponder = PanResponder.create({
+        onStartShouldSetPanResponder: (e, gestureState) => {
+            return true;
+        },
+        onPanResponderEnd: (e, gestureState) => {
+            console.log("pan responder end", gestureState);
+            if (recognizeDrag(gestureState)){
+                Alert.alert(
+                    'Add Favorite',
+                    'Are you sure you wish to add ' + dish.name + ' to favorite?',
+                    [
+                    {text: 'Cancel', onPress: () => console.log('Cancel Pressed'), style: 'cancel'},
+                    {text: 'OK', onPress: () => {props.favorite ? console.log('Already favorite') : props.onPress()}},
+                    ],
+                    { cancelable: false }
+                );
+            }
+            else if (recognizeComment(gestureState)) {
+                props.toggleModal()
+            }
+
+            return true;
+        },
+        onPanResponderGrant: () => {this.view.rubberBand(1000).then(endState => console.log(endState.finished ? 'finished' : 'cancelled'))}
+    })
 
     if ( dish != null ) {
         return (
-            <Card
-                featuredTitle = {dish.name}
-                image = {require('./images/uthappizza.png')}
-            >
-                <Text
-                    style = {{margin: 10}}
+            <Animatable.View animation="fadeInDown" duration={2000} delay={1000} 
+                ref={this.handleViewRef} 
+                {...panResponder.panHandlers}>
+                <Card
+                    featuredTitle = {dish.name}
+                    image = {{uri: baseUrl + dish.image}}
                 >
-                    {dish.description}
-                </Text>
-            </Card>
+                    <Text
+                        style = {{margin: 10}}
+                    >
+                        {dish.description}
+                    </Text>
+                    <View style={{flex: 1, flexDirection: "row", justifyContent: 'center'}}>
+                        <Icon raised reverse name={props.favorite ? 'heart' : 'heart-o'} type="font-awesome" color="#f50" onPress={() => props.favorite ? console.log('Already favorite') : props.markFavorite() }>
+                        </Icon>
+                        <Icon raised reverse name={'pencil'} type="font-awesome" color="#512DA8" onPress = {() => props.toggleModal() }>
+                        </Icon>
+                        <Icon
+                            raised
+                            reverse
+                            name='share'
+                            type='font-awesome'
+                            color='#51D2A8'
+                            style={styles.cardItem}
+                            onPress={() => shareDish(dish.name, dish.description, baseUrl + dish.image)} 
+                        /> 
+                    </View>                                              
+                </Card>
+            </Animatable.View>
         )
     }
     else {
@@ -27,11 +112,42 @@ import {DISHES} from '../shared/dishes';
     }
 }
 
-export default class Dishdetail extends Component  {
-    constructor(props) {
+function RenderComments (props) {
+    const comments = props.comments
+    const renderCommentItem = ({item,index}) => (
+        <View key={index} style={{margin:10}}>
+            <Text style={{fontSize: 14}}>
+                {item.comment}
+            </Text>
+            <Text>
+                <Rating imageSize={12} readonly startingValue={item.rating} />
+            </Text>
+            <Text style={{fontSize: 12}}>
+                {'-- ' + item.author + ', ' + item.date}
+            </Text>
+        </View>
+    )
+
+    return (
+        <Animatable.View animation="fadeInUp" duration={2000} delay={1000}>
+            <Card title="Comments">
+                <FlatList
+                    data = {comments}
+                    renderItem = {renderCommentItem}
+                    keyExtractor = { item => item.id.toString()}
+                >
+                </FlatList>
+            </Card>
+        </Animatable.View>
+    )
+}
+
+class Dishdetail extends Component  {
+
+    constructor(props){
         super(props)
         this.state = {
-            dishes: DISHES
+            showModal: false
         }
     }
 
@@ -39,12 +155,97 @@ export default class Dishdetail extends Component  {
         title: 'Dish Details'
     }
 
+    toggleModal() {
+        this.setState(
+            (state) => ({showModal: !state.showModal})
+        );
+    }
+
+    markFavorite (dishId) {
+        this.props.postFavorite(dishId);
+    }
+
+    handleComment (dishId, rating, comment, author){
+        var date = new Date().toISOString();
+        id       = this.props.comments.comments.length;
+        comment = {
+            id: id,
+            dishId: dishId,
+            rating: rating,
+            comment: comment,
+            author: author,
+            date: date
+        } 
+        this.props.postComment(comment)
+    }
+
+
     render () {
         const dishId = this.props.navigation.getParam('dishId','')
-
+        let author = null
+        let comment = null
+        let rating = null
         return (
-            <RenderDish dish = {this.state.dishes[+dishId]}/>
+            <ScrollView>
+                <RenderDish 
+                    dish = {this.props.dishes.dishes[+dishId]} 
+                    favorite = {this.props.favorites.some(el => el === dishId)}
+                    markFavorite = {() => this.markFavorite(dishId)}
+                    toggleModal  = {() => this.toggleModal()}
+                />
+                <RenderComments comments = {this.props.comments.comments.filter((comment) => comment.dishId === dishId)}/>
+                <Modal animationType = {"slide"} transparent = {false}
+                    visible = {this.state.showModal}
+                    onDismiss = {() => this.toggleModal() }
+                    onRequestClose = {() => this.toggleModal() }>
+                    <View style = {styles.modal}>
+                        <Rating
+                            imageSize = {24}
+                            showRating
+                            onFinishRating={(rt) => rating = rt}
+                            style={{ paddingVertical: 10 }}
+                        />
+                        <Input
+                            placeholder='  Author'
+                            leftIcon={{ type: 'font-awesome', name: 'user' }}
+                            onChangeText={value => author = value}
+                        />
+                        <Input
+                            placeholder='  Comment'
+                            leftIcon={{ type: 'font-awesome', name: 'comment' }}
+                            onChangeText={value => comment = value}
+                        />
+                        <View style = {styles.modalText}>
+                            <Button 
+                                onPress = {() => { this.handleComment(dishId, rating, comment, author);this.toggleModal(); }}
+                                color="#512DA8"
+                                title="Submit"                                
+                            />
+                        </View>
+                        <View style = {styles.modalText}>
+                            <Button 
+                                onPress = { () => this.toggleModal()} 
+                                color="gray"
+                                title="Cancel" 
+                            />
+                        </View>
+                    </View>
+                </Modal>
+            </ScrollView>            
         )
     } 
     
 }
+
+const styles = StyleSheet.create({
+    modal: {
+        justifyContent: 'center',
+        margin: 20
+     },
+     modalText: {
+         fontSize: 18,
+         margin: 10
+     }
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(Dishdetail)
---
+++ components/ReservationComponent.js
@@ -0,0 +1,210 @@
+import React, { Component } from 'react';
+import { Text, View, StyleSheet, Picker, Switch, Button, Alert } from 'react-native';
+import DatePicker from 'react-native-datepicker'
+import * as Animatable from 'react-native-animatable';
+import {Notifications} from 'expo'
+import * as Permissions from 'expo-permissions'
+import * as Calendar from 'expo-calendar'
+
+class Reservation extends Component {
+
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            guests: 1,
+            smoking: false,
+            date: new Date().toISOString(),
+            showModal: false
+        }
+    }
+
+    static navigationOptions = {
+        title: 'Reserve Table',
+    };
+
+    async obtainCalendarPermission() {
+        let permission = await Permissions.getAsync(Permissions.CALENDAR);
+        if (permission.status !== 'granted') {
+            permission = await Permissions.askAsync(Permissions.CALENDAR);
+            if (permission.status !== 'granted') {
+                Alert.alert('Permission not granted to calendar');
+            }
+        }
+        return permission;
+    }
+
+    addReservationToCalendar (date) {
+        if ( this.obtainCalendarPermission().status === 'granted') {
+             Calendar.createEventAsync(
+                '0',
+                {
+                    title: 'Con Fusion Table Reservation' ,
+                    startDate: Date(Date.parse(date)),
+                    endDate:  new Date (Date.parse(date) + 1000*2*3600),
+                    timeZone: 'Asia/Hong_Kong',
+                    location: '121, Clear Water Bay Road, Clear Water Bay, Kowloon, Hong Kong'
+                }
+            )
+        }
+    }
+
+    toggleModal() {
+        this.setState(
+            (state) => ({showModal: !state.showModal})
+        );
+    }
+
+    handleReservation() {
+        console.log(JSON.stringify(this.state));
+        const smoking =  this.state.smoking ? 'Yes' : 'No'
+        Alert.alert (
+            'Your Reservation OK?',
+            'Number of Guests: ' +  String(this.state.guests) + '\n' +
+            'Smoking?: ' + smoking + '\n' +
+            'Date and Time: ' + this.state.date,
+            [
+                {text: 'Cancel', onPress: () => this.resetForm(), style: 'cancel'},
+                {text: 'OK', onPress: () =>  {this.presentLocalNotification(this.state.date); this.resetForm()} },
+            ],
+            { cancelable: false }
+        )
+        this.addReservationToCalendar(this.state.date)
+    }
+
+    resetForm() {
+        this.setState({
+            guests: 1,
+            smoking: false,
+            date: '',
+            showModal: false
+        });
+    }
+
+    async obtainNotificationPermission() {
+        let permission = await Permissions.getAsync(Permissions.USER_FACING_NOTIFICATIONS);
+        if (permission.status !== 'granted') {
+            permission = await Permissions.askAsync(Permissions.USER_FACING_NOTIFICATIONS);
+            if (permission.status !== 'granted') {
+                Alert.alert('Permission not granted to show notifications');
+            }
+        }
+        return permission;
+    }
+
+    async presentLocalNotification(date) {
+        await this.obtainNotificationPermission();
+        Notifications.presentLocalNotificationAsync({
+            title: 'Your Reservation',
+            body: 'Reservation for '+ date + ' requested',
+            ios: {
+                sound: true
+            },
+            android: {
+                sound: true,
+                vibrate: true,
+                color: '#512DA8'
+            }
+        });
+    }
+    
+    render() {
+        return(
+            <Animatable.View animation="zoomInUp" duration={2000}>
+                <View style={styles.formRow}>
+                <Text style={styles.formLabel}>Number of Guests</Text>
+                <Picker
+                    style={styles.formItem}
+                    selectedValue={this.state.guests}
+                    onValueChange={(itemValue, itemIndex) => this.setState({guests: itemValue})}>
+                    <Picker.Item label="1" value="1" />
+                    <Picker.Item label="2" value="2" />
+                    <Picker.Item label="3" value="3" />
+                    <Picker.Item label="4" value="4" />
+                    <Picker.Item label="5" value="5" />
+                    <Picker.Item label="6" value="6" />
+                </Picker>
+                </View>
+                <View style={styles.formRow}>
+                <Text style={styles.formLabel}>Smoking/Non-Smoking?</Text>
+                <Switch
+                    style={styles.formItem}
+                    value={this.state.smoking}
+                    onTintColor='#512DA8'
+                    onValueChange={(value) => this.setState({smoking: value})}>
+                </Switch>
+                </View>
+                <View style={styles.formRow}>
+                <Text style={styles.formLabel}>Date and Time</Text>
+                <DatePicker
+                    style={{flex: 2, marginRight: 20}}
+                    date={this.state.date}
+                    format=''
+                    mode="datetime"
+                    placeholder="select date and Time"
+                    minDate="2017-01-01"
+                    confirmBtnText="Confirm"
+                    cancelBtnText="Cancel"
+                    customStyles={{
+                    dateIcon: {
+                        position: 'absolute',
+                        left: 0,
+                        top: 4,
+                        marginLeft: 0
+                    },
+                    dateInput: {
+                        marginLeft: 36
+                    }
+                    // ... You can check the source to find the other keys. 
+                    }}
+                    onDateChange={(date) => {this.setState({date: date})}}
+                />
+                </View>
+                <View style={styles.formRow}>
+                <Button
+                    onPress={() => this.handleReservation()}
+                    title="Reserve"
+                    color="#512DA8"
+                    accessibilityLabel="Learn more about this purple button"
+                    />
+                </View>
+            </Animatable.View>
+        );
+    }
+
+};
+
+const styles = StyleSheet.create({
+    formRow: {
+      alignItems: 'center',
+      justifyContent: 'center',
+      flex: 1,
+      flexDirection: 'row',
+      margin: 20
+    },
+    formLabel: {
+        fontSize: 18,
+        flex: 2
+    },
+    formItem: {
+        flex: 1
+    },
+    modal: {
+        justifyContent: 'center',
+        margin: 20
+     },
+     modalTitle: {
+         fontSize: 24,
+         fontWeight: 'bold',
+         backgroundColor: '#512DA8',
+         textAlign: 'center',
+         color: 'white',
+         marginBottom: 20
+     },
+     modalText: {
+         fontSize: 18,
+         margin: 10
+     }
+});
+
+export default Reservation;
